16b3a778e3fcdf1ab30e0405d259cd797be994f3
portalbe94 <portalbe94@gmail.com>
2018-05-16 14:10:13 -0700

    ....

--- app/src/main/java/com/example/business/recycleviewjsonexample/MainActivity.java
+++ app/src/main/java/com/example/business/recycleviewjsonexample/MainActivity.java
@@ -7,9 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.android.volley.Request;
@@ -31,13 +34,16 @@ public class MainActivity extends AppCompatActivity implements ExampleAdapter.On
     public static final String EXTRA_CREATOR = "creatorName";
     public static final String EXTRA_LIKES = "likeCount";
 
-
+    Button btn;
+    EditText edt;
+    TextView txtView;
 
 
     private RecyclerView mRecyclerView;
     private ExampleAdapter mExampleAdapter;
     private ArrayList<ExampleItem> mExampleList;
     private RequestQueue mRequestQueue;
+    public static final String BASE_URL = "http://api.walmartlabs.com/v1/search?apiKey=3kgshhj4nr73xrzfmcbthwwb&query=";
 
     private SearchView mSearchView;
 
@@ -46,9 +52,15 @@ public class MainActivity extends AppCompatActivity implements ExampleAdapter.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+
         mRecyclerView = findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        btn = (Button) findViewById(R.id.button2);
+        edt = (EditText) findViewById(R.id.editText);
 
         mExampleList = new ArrayList<>();
 
@@ -56,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements ExampleAdapter.On
 
         mRequestQueue = Volley.newRequestQueue(this);
         //parseJSON();
-        mSearchView.setOnSearchClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -66,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements ExampleAdapter.On
     }
 
     private void parseJSON() {
-        String url = "http://api.walmartlabs.com/v1/search?apiKey=3kgshhj4nr73xrzfmcbthwwb&query=";
+        String url = "http://api.walmartlabs.com/v1/search?apiKey=3kgshhj4nr73xrzfmcbthwwb&query=" + edt.getText();
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
@@ -75,6 +87,7 @@ public class MainActivity extends AppCompatActivity implements ExampleAdapter.On
                         try {
                             JSONArray jsonArray = response.getJSONArray("items");
 
+                            mExampleList.clear();
                             for (int i = 0; i < jsonArray.length(); i++ ) {
                                 JSONObject item = jsonArray.getJSONObject(i);
 //""items": {itemId,parentItemId, name, etc}
